58b001203e84126bbe4013f7137944b7fe6a6cb8
galalon <sgalalon@gmail.com>
2025-11-04 07:58:39 +0200

    multichannel bayer fix

--- dithering/methods/pattern_multiple_channels.py
+++ dithering/methods/pattern_multiple_channels.py
@@ -9,6 +9,9 @@ def pattern_multiple_channels_dither_core(img_cs, palette_cs, color_space='RGB',
     channel_scales = np.array(CHANNEL_NOISE_STD.get(color_space.upper(), [1.0] * C), dtype=np.float32)
 
     pattern_array = generate_pattern(pattern_name=pattern, shape=img_cs.shape, pattern_params=pattern_params)
+    if pattern == 'bayer':
+        pattern_array = (pattern_array - pattern_array.min()) / (pattern_array.max() - pattern_array.min())
+        pattern_array = pattern_array * 2 - 1  # Normalize to [-1, 1]
     out = img_cs.copy()
 
     out += scale * pattern_array * channel_scales
